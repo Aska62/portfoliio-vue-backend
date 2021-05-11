c1df8329ee50bd2442595d571f6dc7ab32154f27
Aska62 <aska.jpg@gmail.com>
2021-05-11 16:50:30 +0900

    added env file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -12,3 +15,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!!!`);
 });
+
+var firebaseConfig = {
+  apiKey: process.env.NODE_APP_API_KEY,
+  authDomain: process.env.NODE_APP_AUTH_DOMAIN,
+  projectId: process.env.NODE_APP_PROJECT_ID,
+  storageBucket: process.env.NODE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.NODE_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.NODE_APP_APP_ID,
+  measurementId: process.env.NODE_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
